9796eb0e2c7a31d3a3e7abc2e2e02fdbf67354af
Mateusz Kaczmarkiewicz <matt327k@gmail.com>
2021-01-30 19:47:34 +0100

    przejcia miedzy poziomami

---
+++ TileVania/Assets/Scripts/LevelExit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            StartCoroutine(LoadNextScene());
+        }
+    }
+
+    private IEnumerator LoadNextScene()
+    {
+        float loadDelay = 2f;
+
+        yield return new WaitForSecondsRealtime(loadDelay);
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
